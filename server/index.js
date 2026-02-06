4d897e96ae7455a98b7e27e55c03bdca12c674ef
kulzhabai.e <kulzhabai.e@sailet.pro>
2026-02-06 23:49:50 +0500

    06.02.2026 7

--- server/index.js
+++ server/index.js
@@ -61,8 +61,8 @@ mongoose
     });
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.text());
 app.use(cookieParser());
 app.use(cors({ 

4c7c2ceb11a74abb0bf5648c91c360410e4eed4c
kulzhabai.e <kulzhabai.e@sailet.pro>
2026-02-08 19:14:33 +0500

    08.02.2026 4

--- client/src/pages/User/ClientNavigator.tsx
+++ client/src/pages/User/ClientNavigator.tsx
@@ -4,6 +4,7 @@ import api from "../../api"
 import { useState } from "react"
 import { useEffect } from "react"
 import navigatorMobile from "../../assets/navigatorMobile.jpg"
+import navigatorDesktop from "../../assets/navigatorDesktop.png"
 import { Switch } from "../../components/User/Switch"
 import { useNavigate } from "react-router-dom"
 import navigatorConsciousnessLibrary from "../../assets/navigatorConsciousnessLibrary.png"
@@ -78,39 +79,48 @@ export const ClientNavigator = () => {
     return (
         <UserLayout>
             <div className="max-h-screen">
-                <BackNav title="Навигатор" />
+                <div className="lg:flex lg:flex-row lg:justify-between lg:items-center lg:pr-5">
+                    <BackNav title="Навигатор" />
+                    <div className="lg:block hidden">
+                        <div className="flex items-center gap-x-4">
+                            <div className="">Показывать описание</div>
+                            <Switch
+                                checked={showDescription}
+                                onChange={() => { 
+                                    updateShowDescription();
+                                }} 
+                            />
+                        </div>
+                    </div>
+                </div>
+                
                 <div className="-mt-2 bg-[#031F23]">
-                    <p className="px-4" dangerouslySetInnerHTML={{ __html: content?.content }}></p>
+                    <p className="px-4 lg:w-1/2" dangerouslySetInnerHTML={{ __html: content?.content }}></p>
                     <div className='relative'>
-                        <div className="relative flex justify-center items-center">
+                        <div className="relative flex justify-center items-center lg:hidden">
                             <img 
                                 src={navigatorMobile} 
                                 alt={"Навигатор"} 
                                 className=' rounded-lg object-top z-10' 
                             />
                         </div>
+                        <div className="relative hidden lg:block justify-center items-center">
+                            <img 
+                                src={navigatorDesktop} 
+                                alt={"Навигатор"} 
+                                className=' rounded-lg object-top z-10' 
+                            />
+                        </div>
                         <div 
-                            className="absolute inset-0 z-10 lg:hidden"
+                            className="absolute inset-0 z-10"
                             style={{
                                 background: 'linear-gradient(to top, #031F2300 80%, #031F23 99%)',
                             }}
                         />
                         <div 
-                            className="absolute inset-0 z-10 lg:hidden"
+                            className="absolute inset-0 z-10"
                             style={{
                                 background: 'linear-gradient(to bottom, #031F2300 60%, #031F23 100%)',
-                            }}
-                        />
-                        <div 
-                            className="absolute inset-0 z-10 hidden lg:block"
-                            style={{
-                                background: 'linear-gradient(to right, #031F2300 70%, #031F23 100%)',
-                            }}
-                        />
-                        <div 
-                            className="absolute inset-0 z-10 hidden lg:block"
-                            style={{
-                                background: 'linear-gradient(to left, #031F2300 70%, #031F23 100%)',
                             }}
                         />
                         <button onClick={() => {
@@ -120,7 +130,7 @@ export const ClientNavigator = () => {
                             } else {
                                 navigate("/client/consciousness-library")
                             }
-                        }} className="absolute top-[19%] left-[28%] w-[116px] h-[40px] z-20">
+                        }} className="absolute top-[19%] left-[25%] w-[116px] h-[40px] z-20 lg:top-[35%] lg:left-[60%] cursor-pointer">
                             <img src={navigatorConsciousnessLibrary} alt={""} className="object-cover w-full h-full" />
                         </button>
                         <button onClick={() => {
@@ -130,7 +140,7 @@ export const ClientNavigator = () => {
                             } else {
                                 navigate("/client/spirit-forge")
                             }
-                        }} className="absolute top-[30%] left-[20%] w-[93px] h-[40px] z-20">
+                        }} className="absolute top-[33%] left-[18%] w-[93px] h-[40px] z-20 lg:top-[50%] lg:left-[42%] cursor-pointer">
                             <img src={navigatorSpiritForge} alt={""} className="object-cover w-full h-full" />
                         </button>
                         <button onClick={() => {
@@ -140,7 +150,7 @@ export const ClientNavigator = () => {
                             } else {
                                 navigate("/client/relationship-workshop")
                             }
-                        }} className="absolute top-[40%] left-[20%] w-[96px] h-[61px] z-20">
+                        }} className="absolute top-[43%] left-[20%] w-[96px] h-[61px] z-20 lg:top-[48%] lg:left-[15%] cursor-pointer">
                             <img src={navigatorRelationshipWorkshop} alt={""} className="object-cover w-full h-full" />
                         </button>
                         <button onClick={() => {
@@ -150,7 +160,7 @@ export const ClientNavigator = () => {
                             } else {
                                 navigate("/client/masters-tower")
                             }
-                        }} className="absolute top-[40%] left-[72%] w-[78px] h-[61px] z-20">
+                        }} className="absolute top-[43%] left-[72%] w-[78px] h-[61px] z-20 lg:top-[63%] lg:left-[77%] cursor-pointer">
                             <img src={navigatorMastersTower} alt={""} className="object-cover w-full h-full" />
                         </button>
                         <button onClick={() => {
@@ -160,7 +170,7 @@ export const ClientNavigator = () => {
                             } else {
                                 navigate("/client/health-lab")
                             }
-                        }} className="absolute top-[53%] left-[7%] w-[107px] h-[61px] z-20">
+                        }} className="absolute top-[56%] left-[7%] w-[107px] h-[61px] z-20 lg:top-[76%] lg:left-[9%] cursor-pointer">
                             <img src={navigatorHealthLab} alt={""} className="object-cover w-full h-full" />
                         </button>
                         <button onClick={() => {
@@ -170,16 +180,16 @@ export const ClientNavigator = () => {
                             } else {
                                 navigate("/client/femininity-gazebo")
                             }
-                        }} className="absolute top-[65%] left-[65%] w-[115px] h-[61px] z-20">
+                        }} className="absolute top-[68%] left-[65%] w-[115px] h-[61px] z-20 lg:top-[74%] lg:left-[86%] cursor-pointer">
                             <img src={navigatorFemininityGazebo} alt={""} className="object-cover w-full h-full" />
                         </button>
                         <button onClick={() => {
                             navigate("/client/beggining-journey")
-                        }} className="absolute top-[72%] left-[22%] w-[99px] h-[61px] z-20">
+                        }} className="absolute top-[75%] left-[22%] w-[99px] h-[61px] z-20 lg:top-[88%] lg:left-[45%] cursor-pointer">
                             <img src={navigatorBegginingJourney} alt={""} className="object-cover w-full h-full" />
                         </button>
                     </div>
-                    <div className="px-4 mt-2 pb-10">
+                    <div className="px-4 mt-2 pb-10 lg:hidden">
                         <div className="flex items-center justify-between">
                             <div className="">Показывать описание</div>
                             <Switch
@@ -255,9 +265,25 @@ export const ClientNavigator = () => {
                                 <X size={24} />
                             </button>
                             <div 
-                                className="[&_h3]:text-xl [&_h3]:font-bold [&_h3]:mb-2 [&_div]:mb-2 [&_span]:font-bold" 
-                                dangerouslySetInnerHTML={{ __html: selectedContent.eventTitle || 'Добавить в календарь' }} 
+                                className="font-medium text-xl text-white" 
+                                dangerouslySetInnerHTML={{ __html: selectedContent.title || "" }} 
                             />
+                            <div className="mt-4 text-sm" dangerouslySetInnerHTML={{ __html: selectedContent.description }}></div>
+                            <div className="">
+                                {selectedContent.content.map((item: ContentItem) => (
+                                    <div key={item.title} className="">
+                                        <p className="mt-3 font-medium text-md text-white" dangerouslySetInnerHTML={{ __html: item.title }}></p>
+                                        <p className="mt-1 text-sm" dangerouslySetInnerHTML={{ __html: item.description }}></p>
+                                    </div>
+                                ))}
+                            </div>
+
+                            <a 
+                                className="block bg-[#C4841D] text-white py-2.5 text-center font-medium rounded-full mt-4 w-full" 
+                                href={selectedContent.link}
+                            >
+                                <p>Перейти</p>
+                            </a>
                         </div>
                     </div>
                 </div>
